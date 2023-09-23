fcc33457aa2f6276581871ebc437f0107cd28ebc
shiva <shivain22@gmail.com>
2023-09-23 13:48:02 +0530

    Processor for excel

--- src/main/java/com/documentmanager/service/DocStoreService.java
+++ src/main/java/com/documentmanager/service/DocStoreService.java
@@ -1,6 +1,7 @@
 package com.documentmanager.service;
 
 import com.documentmanager.service.dto.DocStoreDTO;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -63,4 +64,6 @@ public interface DocStoreService {
      * @param id the id of the entity.
      */
     void delete(Long id);
+
+    void createTable(String tableName, List<String> colNames);
 }
--- src/main/java/com/documentmanager/service/impl/DocProcessor.java
+++ src/main/java/com/documentmanager/service/impl/DocProcessor.java
@@ -2,6 +2,16 @@ package com.documentmanager.service.impl;
 
 import com.documentmanager.domain.DocStore;
 import com.documentmanager.service.dto.DocStoreDTO;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,7 +29,30 @@ public class DocProcessor implements Runnable {
 
     @Override
     public void run() {
-        docStoreDTO.getFileObject();
+        try {
+            long start = System.currentTimeMillis();
+            InputStream stream = new ByteArrayInputStream(docStoreDTO.getFileObject());
+            Workbook workbook = WorkbookFactory.create(stream);
+            Sheet firstSheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = firstSheet.iterator();
+            int count = 0;
+            while (rowIterator.hasNext()) {
+                Row nextRow = rowIterator.next();
+                Iterator<Cell> cellIterator = nextRow.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell nextCell = cellIterator.next();
+                    int columnIndex = nextCell.getColumnIndex();
+                    DataFormatter formatter = new DataFormatter();
+                    String strValue = formatter.formatCellValue(nextCell);
+                }
+            }
+            workbook.close();
+            long end = System.currentTimeMillis();
+            System.out.printf("Import done in %d ms\n", (end - start));
+        } catch (IOException ex1) {
+            System.out.println("Error reading file");
+            ex1.printStackTrace();
+        }
         docStoreDTO.getFileName();
     }
 }
